992f60a71ffff06e4bdaa03c568d931a6f877774
Ezequiel Julian Hurt <ezequiel-julian.hurt@basf.com>
2024-05-30 15:32:33 +0200

    installed sass and ant design

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,19 @@
 /// <reference types="vite/client" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sass from 'sass'
 
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
     globals: true,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass,
+      },
+    },
   },
 })
